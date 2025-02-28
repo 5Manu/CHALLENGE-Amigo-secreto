36085e8f24701afd461794b3025789caadd9f4c2
Manuel <5manuelbravo5@gmail.com>
2025-02-28 19:07:48 -0300

    Se crea las primeras líneas de código para el funcionamiento del proyecto, se deja explicación más detalla en el README.md

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 let amigos = [];
 
+enter();
+
+
+
+
+function agregarAmigo() {
+
+    if (document.getElementById("amigo").value==="") {
+        alert("Por favor escribe el nombre de tu amigo")
+    }
+    else {
+    let lista = document.getElementById("amigo").value
+    console.log(lista);
+    amigos.push(lista);
+    console.log(amigos);
+    limpiar();
+    }
+
+}
+
+function limpiar () {
+    document.getElementById("amigo").value= "";
+    document.getElementById("amigo").focus();
+}
+
+function enter() {
+    document.getElementById("amigo").addEventListener("keydown",
+        function(event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                agregarAmigo();
+            }
+        }          
+            
+    )
+}
+
+
