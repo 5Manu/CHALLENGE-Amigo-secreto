1e85722b00da8fd1f80a8a939f96c247eaa869f7
Manuel <5manuelbravo5@gmail.com>
2025-03-01 19:43:04 -0300

    Se realiza código con funcionamiento para agregar nuevos amigos y crear LI con los nombres en el index.html

--- app.js
+++ app.js
@@ -5,27 +5,18 @@ enter();
 
 
 
-function agregarAmigo() {
 
-    if (document.getElementById("amigo").value==="") {
-        alert("Por favor escribe el nombre de tu amigo")
-    }
-    else {
-    let lista = document.getElementById("amigo").value
-    console.log(lista);
-    amigos.push(lista);
-    console.log(amigos);
-    limpiar();
-    }
 
-}
+
 
 function limpiar () {
+
     document.getElementById("amigo").value= "";
     document.getElementById("amigo").focus();
 }
 
 function enter() {
+
     document.getElementById("amigo").addEventListener("keydown",
         function(event) {
             if (event.key === "Enter") {
@@ -36,5 +27,32 @@ function enter() {
             
     )
 }
+
+
+function agregarAmigo() {
+
+    if (document.getElementById("amigo").value==="") {
+        alert("Por favor escribe el nombre de tu amigo")
+    }
+    else {
+    let lista = document.getElementById("amigo").value;
+    console.log(lista);
+    amigos.push(lista);
+    limpiar();  
+    agregarLista();  
+    }
+}
+
+
+function agregarLista () {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let cuentaAmigos = 0; cuentaAmigos < amigos.length; cuentaAmigos++) {
+        let listaDeAmigos = amigos[cuentaAmigos];
+        console.log(listaDeAmigos);
+        lista.innerHTML += `<li>${listaDeAmigos}</li>`;
+    }
+}
+
 
 
