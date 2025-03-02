3b926ce21b3c372cf95de96b96888656de283f40
Manuel <5manuelbravo5@gmail.com>
2025-03-02 16:38:05 -0300

    Funcionamiento del botón para sortear

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 CHALLENGE Amigo secreto
 28-02-2025:
 Consiste en permitir al usuario agregar los nombres de los participantes y luego escoger aleatoreamente uno de ellos.
-En caso de que intente introducir un número debe arrojar un "alert" indicándole que debe ser el nombre de un participante
+En caso de que intente introducir un  espacio vacío debe arrojar un "alert" indicándole que debe ser el nombre de un participante
 
 Prueba de sincronización entre GIT local y online.
 
--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 let amigos = [];
-
+let lista = document.getElementById("listaAmigos");
+let ganador = document.getElementById("resultado");
 enter();
-
-
-
-
-
 
 
 
@@ -13,19 +9,6 @@ function limpiar () {
 
     document.getElementById("amigo").value= "";
     document.getElementById("amigo").focus();
-}
-
-function enter() {
-
-    document.getElementById("amigo").addEventListener("keydown",
-        function(event) {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                agregarAmigo();
-            }
-        }          
-            
-    )
 }
 
 
@@ -44,9 +27,24 @@ function agregarAmigo() {
 }
 
 
+function enter() {
+
+    document.getElementById("amigo").addEventListener("keydown",
+        function(event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                agregarAmigo();
+            }
+        }          
+            
+    )
+}
+
+
+
 function agregarLista () {
-    let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
+    ganador.innerHTML= "";
     for (let cuentaAmigos = 0; cuentaAmigos < amigos.length; cuentaAmigos++) {
         let listaDeAmigos = amigos[cuentaAmigos];
         console.log(listaDeAmigos);
@@ -54,5 +52,27 @@ function agregarLista () {
     }
 }
 
+function sortearAmigo() {
+    //validar si está vacío el ARRAY amigos
+    if (amigos.length == 0) {
+        //alert("Por favor, primero agrega un amigo");
+        lista.innerHTML="";
+        lista.innerHTML += `<li>Por favor, agrega a un amigo antes de sortear</li>`;        
+    }
+    else {
+        lista.innerHTML= "";
+        ganador.innerHTML= "";
+        let elegido= amigos[Math.floor(Math.random()*amigos.length)];
+        ganador.innerHTML += `<li>${elegido}</li>`;      
+         
+
+    }
+    //Usando el Math.random y Math.floor seleccionar un índice aleatorio
+
+    //utilizar el índice aleatorio para acceder al nombre correspondiente
+
+    //arrojar el nombre seleccionado con el document.getElementByID e innerHTML
+}
+    
 
 
